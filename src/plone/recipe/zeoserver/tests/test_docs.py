d1ed8c0918c57ea36452bd385d6b0d503645e7f4
Hanno Schlichting <hanno@hannosch.eu>
2010-04-24 18:05:52 +0000

    Use one more trick to get fast and reliable test setup
    svn path=/buildout/plone.recipe.zeoserver/trunk/; revision=116069

--- src/plone/recipe/zeoserver/tests/test_docs.py
+++ src/plone/recipe/zeoserver/tests/test_docs.py
@@ -2,44 +2,34 @@
 
 import doctest
 import unittest
-import os
 
+import pkg_resources
 from zc.buildout.testing import buildoutSetUp
 from zc.buildout.testing import buildoutTearDown
 from zc.buildout.testing import install
 from zc.buildout.testing import install_develop
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
-
 
 def setUp(test):
     buildoutSetUp(test)
     install_develop('plone.recipe.zeoserver', test)
-    install('transaction', test)
-    install('ZConfig', test)
-    install('ZODB3', test)
-    install('ZopeUndo', test)
-    install('zc.lockfile', test)
     install('zc.recipe.egg', test)
-    install('zdaemon', test)
-    install('zope.event', test)
-    install('zope.exceptions', test)
-    install('zope.interface', test)
     install('zope.mkzeoinstance', test)
-    install('zope.proxy', test)
+    install('ZopeUndo', test)
+    dependencies = pkg_resources.working_set.require('ZODB3')
+    for dep in dependencies:
+        try:
+            install(dep.project_name, test)
+        except OSError:
+            # Some distribution are installed multiple times
+            pass
 
 
 def test_suite():
     suite = []
     flags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
-    # filtering files on extension
-    docs = [os.path.join(current_dir, doc) for doc in
-            os.listdir(current_dir) if doc.endswith('.txt')]
-
-    for test in docs:
-        suite.append(doctest.DocFileSuite(test, optionflags=flags,
-                    setUp=setUp, tearDown=buildoutTearDown,
-                    module_relative=False))
+    suite.append(doctest.DocFileSuite('zeoserver.txt', optionflags=flags,
+                 setUp=setUp, tearDown=buildoutTearDown))
 
     return unittest.TestSuite(suite)
