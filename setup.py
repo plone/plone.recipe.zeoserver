654a2df5d17e498924133b3b4c1058cf08cc8a5b
Peter Mathis <peter.mathis@kombinat.at>
2023-04-27 16:14:55 +0200

    define python_requires

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: Implementation",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
     packages=find_packages("src"),
     include_package_data=True,
     package_dir={"": "src"},
